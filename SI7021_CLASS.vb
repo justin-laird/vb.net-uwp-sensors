6bef7aaad6e63b00618b606591046caabddc7f6a
justin-laird <56347667+justin-laird@users.noreply.github.com>
2019-10-09 08:22:35 -0500

    Update SI7021_CLASS.vb

--- SI7021_CLASS.vb
+++ SI7021_CLASS.vb
@@ -1,10 +1,9 @@
-﻿Imports Windows.Devices.I2C
+Imports Windows.Devices.I2C
 
 Public Structure SI7021_Data
-    Public TEMPERATURE As Byte
-    Public HUMIDITY As Byte
-    Public TEMP_INT As Decimal
-    Public HUM_INT As Decimal
+    Public TEMPERATURE As Decimal
+    Public HUMIDITY As Decimal
+    Public TIMESTAMP As Date
 End Structure
 
 Public Class SI7021_CLASS
@@ -14,18 +13,10 @@ Public Class SI7021_CLASS
     Private Const SI7021_ADDR As Byte = &H40
 
     Private Const MEASURE_RH_HOLD_MASTER As Byte = &HE5
-    'Private Const MEASURE_RH_NO_HOLD_MASTER As Byte = &HF5
     Private Const MEASURE_TEMP_HOLD_MASTER As Byte = &HE3
-    'Private Const MEASURE_TEMP_NO_HOLD_MASTER As Byte = &HF3
     Private Const READ_TEMP_FROM_PREV_RH As Byte = &HE0
     Private Const RESET As Byte = &HFE
-    'Private Const WRITE_RH_T_USER_REG As Byte = &HE6
     Private Const READ_RH_T_USER_REG As Byte = &HE7
-    'Private Const WRITE_HEATER_CTRL_REG As Byte = &H51
-    'Private Const READ_HEATER_CTRL_REG As Byte = &H11
-    'Private Const READ_ID_BYTE_1 As Byte = &HFA
-    'Private Const READ_ID_BYTE_2 As Byte = &HFC
-    'Private Const READ_FIRMWARE_REV As Byte = &H4
 
     Public Async Function Initialize_SI7021_Async() As Task
         Dim settings As New I2cConnectionSettings(SI7021_ADDR)
@@ -43,25 +34,21 @@ Public Class SI7021_CLASS
 
     Public Function Read() As SI7021_Data
         Dim PROBE_DATA As New SI7021_Data()
-        Dim readBuf As Byte() = New Byte(1) {}
+        Dim READ_BUFFER As Byte() = New Byte(1) {}
         Dim RAW_NUM As Integer
 
         SI7021_SENSOR.Write(New Byte() {RESET})
         Threading.Thread.Sleep(50)
 
-        SI7021_SENSOR.WriteRead(New Byte() {MEASURE_RH_HOLD_MASTER}, readBuf)
+        SI7021_SENSOR.WriteRead(New Byte() {MEASURE_RH_HOLD_MASTER}, READ_BUFFER)
+        RAW_NUM = BitConverter.ToInt16(READ_BUFFER, 0)
+        PROBE_DATA.HUMIDITY = ((125 * RAW_NUM) / 65536) - 6
 
-        RAW_NUM = Convert.ToInt32(BitConverter.ToString(readBuf, 0).Replace("-", ""), 16)
+        SI7021_SENSOR.WriteRead(New Byte() {READ_TEMP_FROM_PREV_RH}, READ_BUFFER)
+        RAW_NUM = BitConverter.ToInt16(READ_BUFFER, 0)
+        PROBE_DATA.TEMPERATURE = ((175.72 * RAW_NUM) / 65536) - 46.85
 
-        PROBE_DATA.HUMIDITY = readBuf(1)
-        PROBE_DATA.HUM_INT = ((125 * RAW_NUM) / 65536) - 6
-
-        SI7021_SENSOR.WriteRead(New Byte() {READ_TEMP_FROM_PREV_RH}, readBuf)
-
-        RAW_NUM = Convert.ToInt32(BitConverter.ToString(readBuf, 0).Replace("-", ""), 16)
-
-        PROBE_DATA.TEMPERATURE = readBuf(1)
-        PROBE_DATA.TEMP_INT = ((175.72 * RAW_NUM) / 65536) - 46.85
+        PROBE_DATA.TIMESTAMP = Date.Now
 
         Return PROBE_DATA
 
